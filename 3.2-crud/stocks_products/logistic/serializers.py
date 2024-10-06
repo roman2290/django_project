83eb34fb1cd80056427e1ffba1467535b8ea0998
Row <roman.22.04@yandex.ru>
2024-10-06 19:35:44 +0700

    Initial commit

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -3,62 +3,42 @@ from .models import Product, Stock, StockProduct
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для продукта
     class Meta:
         model = Product
-        fields = ['id', 'title', 'description']
-
-
+        fields = '__all__'
+        #['title', 'description']
+        
 class ProductPositionSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для позиции продукта на складе
     class Meta:
         model = StockProduct
-        fields =  ['id', 'product', 'quantity', 'price']
-       
-
+        fields = ['product', 'quantity', 'price']
+        
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
     class Meta:
         model = Stock
-        fields = ['id', 'address', 'products', 'positions']
-
-    # настройте сериализатор для склада
-
+        fields = ['address', 'positions']
 
     def create(self, validated_data):
-        # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
-        # создаем склад по его параметрам
         stock = super().create(validated_data)
         for position in positions:
-            StockProduct.objects.create(stock=stock, **position)
+            StockProduct.objects.get_or_create(stock=stock, **position)
         return stock
-        
-  
-
-        # здесь вам надо заполнить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
    
-
     def update(self, instance, validated_data):
-        # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
-
-        # обновляем склад по его параметрам
         stock = super().update(instance, validated_data)
-
-        # здесь вам надо обновить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
         for position in positions:
-            StockProduct.objects.update_or_create(
+            obj, created = StockProduct.objects.update_or_create(
                 defaults={
                     'quantity': position['quantity'],
                     'price': position['price']
                 },
-                product=position['product'],
+                product=['product'],
                 stock=stock)
-
         return stock
+
+
+       
--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,9 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.filters import SearchFilter
-from logistic.models import Product, Stock
+from logistic.models import Product, Stock, StockProduct
 from logistic.serializers import ProductSerializer, StockSerializer
 from django_filters.rest_framework import DjangoFilterBackend
-
 
 
 
@@ -14,35 +13,14 @@ class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
     filter_backends = [SearchFilter]
     search_fields = ['title', 'description']
-    # filter_backends = [BaseFilterBackend]
-    # filterset_fields = ['products']
-    # # при необходимости добавьте параметры фильтрации
-    # def list(self, request):
-    #     review =  ProductSerializer(data = request.data)
-    #     if review.is_valid():
-    #         review.save()
-    #     return Response(review.data)
-
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
-    # при необходимости добавьте параметры фильтрации
-    # filter_backends = [SearchFilter]
-    # SearchFilter.search_param = 'products'
-    # search_fields = ['products__title', 'products__description']
-    #filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['products']
+    filter_backends = [DjangoFilterBackend]
+    filter_fields = ['products']
 
-    # def patch(self, request, pk):
-    #     sen_sor = Product.objects.get(pk=pk)
-    #     seri_alizer = ProductPositionSerializer(sen_sor, data = request.data)
-    #     if seri_alizer.is_valid():
-    #         seri_alizer.save()
-    #     return Response(seri_alizer.data)
 
-    # def post(self, request):
-    #     review = StockSerializer(data=request.data)
-    #     if review.is_valid():
-    #         review.save()
-    #     return Response(review.data)
+
+
+
